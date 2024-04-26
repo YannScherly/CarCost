80ff27b349ea4ae7180ff6cb4ee7041263458c2b
YannScherly <126610036+YannScherly@users.noreply.github.com>
2024-04-26 11:46:48 +0200

    CarCost.py

--- CarCost.py
+++ CarCost.py
@@ -6,10 +6,18 @@ import numpy as np
 import warnings
 warnings.filterwarnings('ignore')
 
-from shapedata.py import process_car_data
-#run a simplified version of shapedata.py cause we didn't know how to refer to it
-car_df = pd.read_csv('CarPrice.csv')
-processed_car_df = process_car_data(car_df)
+car_df=pd.read_csv('CarPrice.csv')
+car_df.head()
+car_df.shape
+car_df.duplicated().sum()
+car_df.drop_duplicates(inplace= True)
+car_df['Brand Name']=car_df['CarName'].str.split(' ').str.slice(0,1).str.join('')
+car_df['Model']=car_df['CarName'].str.split('').str.slice(2,3).str.join('')
+car_df['Model name']=car_df['CarName'].str.split(' ').str.slice(2,3).str.join('')
+car_df.rename(columns={'carname':'Model name'},inplace=True)
+car_df.drop('Model name',axis=1,inplace=True)
+car_df=car_df.iloc[:,[26,2,25,3,5,21,24]]
+
 
 
  
