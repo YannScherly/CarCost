c1d26412907f6deab397aaa6860df3f277c00853
YannScherly <126610036+YannScherly@users.noreply.github.com>
2024-05-10 09:56:29 +0200

    Update CarCost.py

--- CarCost.py
+++ CarCost.py
@@ -2,23 +2,14 @@
 import streamlit as st
 from streamlit.logger import get_logger
 import pandas as pd
-import matplotlib.pyplot as plt
 import numpy as np
 import warnings
 warnings.filterwarnings('ignore')
 
-car_df=pd.read_csv('CarPrice.csv')
-car_df.head()
-car_df.duplicated().sum()
-car_df.drop_duplicates(inplace= True)
-car_df['Brand Name']=car_df['CarName'].str.split(' ').str.slice(0,1).str.join('')
-car_df['Model']=car_df['CarName'].str.split('').str.slice(2,3).str.join('')
-car_df['Model name']=car_df['CarName'].str.split(' ').str.slice(2,3).str.join('')
-car_df.rename(columns={'carname':'Model name'},inplace=True)
-car_df.drop('Model name',axis=1,inplace=True)
-car_df=car_df.iloc[:,[26,2,25,3,5,21,24]]
+from process_car_data import process_car_data
 
-
+car_df = pd.read_csv('CarPrice.csv')
+processed_car_df = process_car_data(car_df)
 
  
  
