659777c7510a54c7de8507e36febe824042613d5
YannScherly <126610036+YannScherly@users.noreply.github.com>
2024-05-10 10:04:55 +0200

    Update CarCost.py

--- CarCost.py
+++ CarCost.py
@@ -5,10 +5,10 @@ import pandas as pd
 import numpy as np
 import warnings
 warnings.filterwarnings('ignore')
-
+car_df = pd.read_csv('CarPrice.csv')
 from shapedata import process_car_data
 
-car_df = pd.read_csv('CarPrice.csv')
+
 processed_car_df = process_car_data(car_df)
 
  
