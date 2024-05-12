d89cf449664481ddb15246ebe6b65b0c5164caef
YannScherly <126610036+YannScherly@users.noreply.github.com>
2024-05-12 12:00:59 +0200

    CarCost.py

--- CarCost.py
+++ CarCost.py
@@ -1,4 +1,4 @@
-
+d
 import streamlit as st
 import pandas as pd
 import numpy as np
@@ -50,8 +50,11 @@ st.write("Infos regarding", model, "->","price:", price, "Fueltype:", fueltype, 
 # Create the user interface
 
 year = st.number_input("Enter the year of the car", min_value=2005, max_value=2024)
+year = float(year)
 kmdrivenperyear = st.number_input('Average yearly kilometers driven', min_value=0)
-petrolprice = st.number_input('Enter the actual petrol price in frs', min_value=0.5, max_value=10, value=1.5)
+kmdrivenperyear = float(kmdrivenperyear)
+petrolprice = st.number_input('Enter the actual petrol price in frs', min_value=0.5, max_value=10.0, value=1.5)
+petrolprice = float(petrolprice
 st.header('Personal details')
 age = st.number_input('Age', min_value=16)
 typeofdriver = st.selectbox('How would you describe your driving style?',['Ecological','Normal','Aggressive'])
@@ -60,15 +63,28 @@ options_mapping_driver = {
     'Normal': 8,
     'Aggressive': 15,
 }
+typeofdriver_numeric = options_mapping_driver[typeofdriver]
+typeofdriver = float(typeofdriver_numeric)
 typeofinsurance = st.selectbox('Which type of insurance would you subscribe?', ['Legal Minimum', 'Partially Covered', 'Fully Insured']) 
 options_mapping_insurance = {
-    'Legal minimum': 900,
+    'Legal Minimum': 900,
     'Partially Covered': 1500,
-    'Fully Covered': 2500,
+    'Fully Insured': 2500,
 }
+typeofinsurance_numeric = options_mapping_driver[typeofinsurance]
+typeofinsurance = float(typeofinsurance_numeric)
 monthsofusage = st.selectbox('For how many months are you planning on using the selected car?', ['12', '24', '36', '48'])
+monthsofusage = float(monthsofusage)
+
+def predict_price(year, kmdrivenperyear, petrolprice, typeofdriver, typeofinsurance, price, mpg, monthsofusage):
+    return ((2024 - year) * 50 + kmdrivenperyear * 0.1 + kmdrivenperyear * petrolprice * (2.35/mpg)) / 100 + price * 0.01 + typeofinsurance + typeofdriver * 100
 
 
+# Add a button to trigger the prediction
+
+if st.button('Predict Price'):
+    price_predicted = predict_price(year, kmdrivenperyear, petrolprice, typeofdriver, typeofinsurance, price, mpg, monthsofusage)
+    st.success(f'You can expect, on average, {price_predicted:,.0f} Swiss Francs for charges, per month, for your car.')
 
 
 
